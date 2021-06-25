78b132930747b6c9ea354343c3015fea7502fc86
native-eng-extra-oz <86416712+native-eng-extra-oz@users.noreply.github.com>
2021-06-25 16:46:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var en = {
     'about':'Hi, my name is Joshua, my wife and I decided to open English courses to help people with this significant language!' +
         '                so follow the link and register.' +
         '                The places are limited.',
-    'geo':'We are in Orekhovo-Zuyevo',
+    'geo':'We are in Orekhovo-Zuyevo not far from the city center',
     'eng-age': 'We have 17 years of experience in teaching English.',
     'ps':'We ready and want to help people to know language better!',
     'phrase': 'You can never understand one language until you understand at least two!"'
@@ -18,7 +18,7 @@ var ru = {
     'application': 'Заявление',
     'about':'Привет, меня зовут Джошуа, мы с женой решили открыть курсы английского языка, в Орехово- Зуево, чтобы помочь людям с столь значительным в нашей жизни языком!\n' +
         'Скорее переходите и регистрируйтесь, так как количество мест ограничено!',
-    'geo':'Мы находимсяв Орехово-Зуево',
+    'geo':'Мы находимся в Орехово-Зуево недалеко от центра города',
     'eng-age': 'Мы уже обучаем Английскому более чем 17 лет.',
     'ps':'Мы готовы и хотим помочь людям лучше знать язык!',
     'phrase': 'Вы никогда не сможете понять один язык, пока не поймете хотя бы два!"'
